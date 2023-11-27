fe0166774a4cf660ee40f74f8a1190b07a7646ee
Lynfen <naelmocquard77@gmail.com>
2023-11-27 15:03:48 +0100

    fin strcpy

--- day_three/ft_strcpy.c
+++ day_three/ft_strcpy.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-char		ft_strcpy(char *dest, char *src)
+void ft_putchar(char c)
+{	
+		write(1, &c, 1);	
+}
+
+int     ft_strlen(char *str)
+{
+	    int i = 1;
+	        while(str[i] != '\0') {
+			            i++;
+				        }
+		    return (i);
+}
+
+char		*ft_strcpy(char *dest, char *src)
 
 {
-	*src = *dest;
+	int i;
+	for (i = 0; src[i] != '\0'; i++)
+
+	{	dest[i] = src[i];
+}	
+	dest[i] = '\0';
+	return (dest);
 }
 
 int main ()
 
 {
-	char caca="caca";
-	char pipi;
-	ft_strcpy(&caca, &pipi);
+
+	char *caca = malloc(sizeof(char*));
+	caca = "zZz";
+	char *pipi = malloc(sizeof(char) * ft_strlen(caca));
+       pipi = ft_strcpy(pipi, caca);
 	printf("%s", pipi);
 	return (0);
 }
